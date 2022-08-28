323441b772dfa78e0858a1c2fd5b894b8057ebeb
ViktoryGul <gul.viktory@gmail.com>
2022-08-28 20:55:58 +0300

    Changed Locators (without class).

--- src/main/java/BaseActions.java
+++ src/main/java/BaseActions.java
@@ -10,5 +10,4 @@ public class BaseActions {
         this.driver = driver;
         this.wait = wait;
     }
-
 }
--- src/main/java/Locators.java
+++ src/main/java/Locators.java
@@ -8,11 +8,11 @@ public class Locators {
     public static final By NEXT_BUTTON = By.xpath("//button[text()='Next'][@data-action='next-page']");
     public static final By TEXT_FIELD_USER_NAME = By.cssSelector("input[id='nickname']");
     public static final By BIRTH_DATE_DROP_DAWN_DAY = By.xpath("//div[@id='daySelect']");
-    public static final By BIRTH_DATE_DROP_DAWN_SELECTED_DAY = By.xpath("//ul[@class='dropdown-menu']//a[@data-value='19']");
+    public static final By BIRTH_DATE_DROP_DAWN_SELECTED_DAY = By.xpath("//ul//a[@data-value='19']");
     public static final By BIRTH_DATE_DROP_DAWN_MONTH = By.xpath("//div[@id='monthSelect']");
-    public static final By BIRTH_DATE_DROP_DAWN_SELECTED_MONTH = By.xpath("//ul[@class='dropdown-menu']//a[@data-value='11'][text()='Dec']");
+    public static final By BIRTH_DATE_DROP_DAWN_SELECTED_MONTH = By.xpath("//ul//a[@data-value='11'][text()='Dec']");
     public static final By BIRTH_DATE_DROP_DAWN_YEAR = By.xpath("//div[@id='yearSelect']");
-    public static final By BIRTH_DATE_DROP_DAWN_SELECTED_YEAR = By.xpath("//ul[@class='dropdown-menu']//a[@data-value='2000'][text()='2000']");
+    public static final By BIRTH_DATE_DROP_DAWN_SELECTED_YEAR = By.xpath("//ul//a[@data-value='2000'][text()='2000']");
     public static final By TEXT_FIELD_USER_PHONE = By.cssSelector("input[name='data[phone]']");
     public static final By CONFIRMATION_CHECKBOX = By.xpath("//input[@id='confirmation']");
     public static final By LOCATION_NAME_AUTOCOMPLETE = By.xpath("//input[@name='region_name']");
