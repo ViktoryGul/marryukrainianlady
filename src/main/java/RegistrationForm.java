389fc10a7489ad2d0e4b72059e32786ac27d47c7
ViktoryGul <gul.viktory@gmail.com>
2022-09-10 22:54:08 +0300

    Created GalleryTests, added select & mouseClick

--- src/main/java/RegistrationForm.java
+++ src/main/java/RegistrationForm.java
@@ -1,6 +1,5 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class RegistrationForm extends BaseActions {
---
+++ src/test/java/GalleryTests.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+
+public class GalleryTests extends BaseUI{
+
+    @Test
+    public void testSortPictures1(){
+        mainPage.clickHowItWorksLink();
+        howItWorksPage.clickGalleryLink();
+        galleryPage.selectDropDownByText(Locators.SORT_DROP_DOWN, Data.registrationDate);
+    }
+
+    @Test
+    public void testSortPictures2(){
+        mainPage.clickHowItWorksLink();
+        howItWorksPage.clickGalleryLink();
+        galleryPage.selectDropDownByIndex(Locators.SORT_DROP_DOWN, 3);
+    }
+
+    @Test
+    public void testSortPictures3(){
+        mainPage.clickHowItWorksLink();
+        howItWorksPage.clickGalleryLink();
+        galleryPage.selectDropDownByValue(Locators.SORT_DROP_DOWN, Data.registrationDateValue);
+    }
+}
